11a6089ceeed1b654f2edeae934e78dce4d3180a
Mabeya Sure <suremabeya@gmail.com>
2023-12-29 17:50:42 +0300

    Fibonacci Series

--- fibanacci.py
+++ fibanacci.py
@@ -1,16 +1,23 @@
 # fibanacci series of any number that a user might need
 
 def fib(n):
-    a=0
-    b=1
+    a = 0
+    b = 1
 
     print(a)
     print(b)
-    for i in range(2,n):
-        c=a+b
-        a=b
-        b=c
+    for i in range(2, n):
+        c = a + b
+        a = b
+        b = c
         print(c)
 
-n=int(input("Enter the number to obtain fibonaccii: "))
-fib(n)
+
+n = int(input("Enter the number to obtain fibonaccii: "))
+
+if n<0:
+    print('Please a value greater than 1')
+
+else:
+    print('Fibonacci Series of {}'.format(n))
+    fib(n)
