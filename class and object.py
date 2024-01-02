4278d5ef4ecfa7a72813bf6c2478af681df13d54
Mabeya Sure <suremabeya@gmail.com>
2024-01-02 13:24:17 +0300

    Class and object in python

---
+++ class and object.py
@@ -0,0 +1,12 @@
+# class and object
+class Person:
+    def details(self):
+        print('Sure', 23, 'Python')
+
+
+person1 = Person()
+
+Person.details(person1)
+
+# best approach
+person1.details()
